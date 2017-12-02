f0722ceca62fd11a93a854f79272a53c457e160e
Brian\Admin <arthurbrian1@hotmail.com>
2017-12-02 17:45:08 -0500

    Google Maps plugin

--- nequi/src/pages/home/home.ts
+++ nequi/src/pages/home/home.ts
@@ -8,15 +8,25 @@ import { BarcodeScanner,BarcodeScannerOptions} from '@ionic-native/barcode-scann
 export class HomePage {
 
   options: BarcodeScannerOptions;
+  results:{};
+
 
   constructor(private barcode:BarcodeScanner,public navCtrl: NavController) {
     
   }
 
   async scanBarcode(){
-    const results=await this.barcode.scan();
+    this.options={
+      prompt:"Acerca un codigo Qr para pagar y adquirir tus Puntos"
+    }
+    this.results=await this.barcode.scan(this.options);
+
     //console.log(results);
     
+  }
+
+  async encodeData(){
+    const results =await this.barcode.encode(this.barcode.Encode.TEXT_TYPE,'test');
   }
 }
 
